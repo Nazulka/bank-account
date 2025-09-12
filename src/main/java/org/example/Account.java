ba2ebe08049ea10d91701d8c4833d351cb20e932
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-09-12 22:48:56 +0100

    Initial commit, create Account class

---
+++ src/main/java/org/example/Account.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Account {
+    public String accountNumber;
+    private double balance;
+    private String pin;
+
+
+    // parametrized constructor
+    public Account(String accountNumber, double initialBalance, String pin) {
+        this.accountNumber=accountNumber;
+        this.balance=initialBalance;
+        this.pin=pin;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+
+}
